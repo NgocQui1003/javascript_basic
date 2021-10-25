b20d83a6eddb18afd2d6317583eecfd592f0d210
qui <nguyenngocquiuit@gmail.com>
2021-10-25 22:28:17 +0700

    the way search what you do not know

---
+++ video5/video5.js
@@ -0,0 +1,4 @@
+//phần nội dung này chia sẽ cách tìm một cái j đó mình không biết
+// lên google dịch dịch qua tiếng anh "......."
+// sau đó qua google và gõ "js "......""
+// lên W3 School hoặc là developer.mozilla.org
