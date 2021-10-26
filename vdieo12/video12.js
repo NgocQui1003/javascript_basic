af427b5d87c817d96ef7aabeb21805bd99c68f49
qui <nguyenngocquiuit@gmail.com>
2021-10-26 15:50:08 +0700

    source code loop while

---
+++ vdieo12/video12.js
@@ -0,0 +1,16 @@
+// WHILE, DO WHILE
+let arr = ['qui', 'hue', 'banh beo', 'de thuong']
+
+// đối với dòng lập for
+for(let i =0; i < arr.length; i++)
+{
+    console.log('gia tri trong mang voi i = ', i , 'la: ' , arr[i]);
+}
+
+// đối với vòng lập while
+let i = 0;
+while(i < arr.length)
+{
+    console.log('gia tri mang trong vong lap while voi i = ', i , 'la: ' , arr[i]);
+    i++;
+}
