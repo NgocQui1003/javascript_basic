077ef6203552ace816e016315f5ffe9f35d80e10
qui <nguyenngocquiuit@gmail.com>
2021-10-26 16:19:32 +0700

    source code if else and break continute

---
+++ video13/video13.js
@@ -0,0 +1,13 @@
+// IF ELSE
+let arr = ['qui', 'hue', 'banh beo', 'de thuong']
+let i = 0 ;
+while(i < arr.length)
+{
+    if(arr[i].length === 3)
+    {
+        console.log('Chu co do dai ba ki tu la: ', arr[i]);
+    }
+    i++;
+}
+
+// NỘI DUNG BREAK AND CONTINUTE XEM TRÊN W3 SCHOOL
