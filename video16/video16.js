8b79646600bb4a6416f4cdb474660c2bdb66fed7
qui <nguyenngocquiuit@gmail.com>
2021-11-09 07:52:34 +0700

    Content of the unit Function

---
+++ video16/video16.js
@@ -0,0 +1,11 @@
+//function
+// () input paramater
+// Bài này học về cách tạo một cái function làm sao cho tốt và hướng dẫn đầu tiên
+// nếu biết chi tiết thì lên W3C tìm hiểu thêm
+function sum(a,b){
+    return a+b;
+}
+console.log('sum a + b = ', sum(9,10))
+
+let c = sum(4,5)
+console.log('sum a + b =', c)
